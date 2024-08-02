71ba208153148aabcf5bf3ac7d65c3a6ef5c1341
mulanisam <mulanisam55@gmail.com>
2024-08-02 20:26:20 +0530

    backend url update

--- src/components/common/axiosConfig.js
+++ src/components/common/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'https://scc-backend-production.up.railway.app:8080',
+    baseURL: 'http://scc-backend-production.up.railway.app:8080',
 });
 
 export default axiosInstance;
