53651c7e6c8b3b3967e77f30b73d9b019292b295
mulanisam <mulanisam55@gmail.com>
2024-08-02 20:00:36 +0530

    backend url update for Railway cloud

--- src/components/common/axiosConfig.js
+++ src/components/common/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'scc-backend-production.up.railway.app',
 });
 
 export default axiosInstance;
--- src/components/service/UserService.js
+++ src/components/service/UserService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 class UserService{
-    static BASE_URL = "http://localhost:8080"
+    static BASE_URL = "scc-backend-production.up.railway.app:8080"
 
     static async login(username, password){
         try{
