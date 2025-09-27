0d5c58a4ee726707756c953d02bf7c51eb5af6c5
Samir Mulani <mulanisam55@gmail.com>
2025-09-27 19:38:08 +0530

    deployment changes

--- src/components/common/axiosConfig.js
+++ src/components/common/axiosConfig.js
@@ -1,4 +1,4 @@
 
-export const API_BASE_URL = 'http://localhost:8080'; // Replace with your actual API base URL
+export const API_BASE_URL = `${process.env.REACT_APP_API_URL}`;//'http://localhost:8080'; // Replace with your actual API base URL
 
 
